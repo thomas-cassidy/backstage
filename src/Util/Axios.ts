79b7e4c9bb0cfdcb87e86a7e4f392bdb3731a12d
Tom Cassidy <69700494+thomas-cassidy@users.noreply.github.com>
2023-11-29 13:31:24 +0000

    updated some packages.

--- src/Util/Axios.ts
+++ src/Util/Axios.ts
@@ -6,68 +6,69 @@ import { API_URI } from "./InitialState";
 import { refreshAccessToken } from "./RefreshAccessToken";
 
 export const AXIOS_API = axios.create({
-  baseURL: API_URI,
-  timeout: 10000,
+    baseURL: API_URI,
+    timeout: 5000,
 });
 
 export const useInterceptors = (): number[] => {
-  const state = useAppSelector((state) => state);
-  const dispatch = useAppDispatch();
-  const [reqInterceptor, setReqInterceptor] = useState<number>(0);
-  const [resInterceptor, setResInterceptor] = useState<number>(0);
+    const state = useAppSelector((state) => state);
+    const dispatch = useAppDispatch();
+    const [reqInterceptor, setReqInterceptor] = useState<number>(0);
+    const [resInterceptor, setResInterceptor] = useState<number>(0);
 
-  useEffect(() => {
-    AXIOS_API.interceptors.request.eject(reqInterceptor);
-    AXIOS_API.interceptors.response.eject(resInterceptor);
+    useEffect(() => {
+        AXIOS_API.interceptors.request.eject(reqInterceptor);
+        AXIOS_API.interceptors.response.eject(resInterceptor);
 
-    setReqInterceptor(() =>
-      AXIOS_API.interceptors.request.use(
-        async (config) => {
-          //   console.log("request interceptor");
-          if (!config.headers || !config.headers["x-access-token"]) {
-            config.headers = {
-              "x-access-token": state.auth.ACCESS_TOKEN,
-            };
-          }
-          return config;
-        },
-        (error) => {
-          Promise.reject(error);
-        }
-      )
-    );
+        setReqInterceptor(() =>
+            AXIOS_API.interceptors.request.use(
+                async (config) => {
+                    //   console.log("request interceptor");
+                    if (!config.headers || !config.headers["x-access-token"]) {
+                        config.headers = {
+                            "x-access-token": state.auth.ACCESS_TOKEN,
+                        };
+                    }
+                    return config;
+                },
+                (error) => {
+                    Promise.reject(error);
+                }
+            )
+        );
 
-    setResInterceptor(() =>
-      AXIOS_API.interceptors.response.use(
-        (response) => {
-          return response;
-        },
-        async (error: AxiosError) => {
-          console.log("response interceptor err", error.message);
+        setResInterceptor(() =>
+            AXIOS_API.interceptors.response.use(
+                (response) => {
+                    return response;
+                },
+                async (error: AxiosError) => {
+                    console.log("response interceptor err", error.message);
 
-          const originalRequest = error.config as typeof error.config & {
-            _retry?: boolean;
-          };
+                    const originalRequest =
+                        error.config as typeof error.config & {
+                            _retry?: boolean;
+                        };
 
-          if (!originalRequest._retry) {
-            console.log("retry time");
-            originalRequest._retry = true;
+                    if (!originalRequest._retry) {
+                        console.log("retry time");
+                        originalRequest._retry = true;
 
-            const ACCESS_TOKEN = await refreshAccessToken();
-            dispatch(SET_ACCESS_TOKEN(ACCESS_TOKEN));
-            console.log("access token changed to", ACCESS_TOKEN);
+                        const ACCESS_TOKEN = await refreshAccessToken();
+                        dispatch(SET_ACCESS_TOKEN(ACCESS_TOKEN));
+                        console.log("access token changed to", ACCESS_TOKEN);
 
-            originalRequest.headers = {
-              ...originalRequest.headers,
-              "x-access-token": ACCESS_TOKEN,
-            };
-            return AXIOS_API(originalRequest);
-          }
-          return error.response;
-        }
-      )
-    );
-  }, [state.auth.ACCESS_TOKEN]);
+                        originalRequest.headers = {
+                            ...originalRequest.headers,
+                            "x-access-token": ACCESS_TOKEN,
+                        };
+                        return AXIOS_API(originalRequest);
+                    }
+                    return error.response;
+                }
+            )
+        );
+    }, [state.auth.ACCESS_TOKEN]);
 
-  return [reqInterceptor, resInterceptor];
+    return [reqInterceptor, resInterceptor];
 };
